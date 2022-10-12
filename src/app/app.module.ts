ab9ad7b041593c3735d326bfb5c14cf30bacd974
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-10-12 16:18:56 +0100

    Empty graph component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QueryTableComponent } from './query-table/query-table.component';
 import { QueryGraphComponent } from './query-graph/query-graph.component';
+import { GraphComponent } from './graph/graph.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QueryTableComponent,
-    QueryGraphComponent
+    QueryGraphComponent,
+    GraphComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/graph/graph.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.scss']
+})
+export class GraphComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
