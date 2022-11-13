6eb976976ebd7f00cea80fbdb35927941d9af775
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-11-13 16:02:49 +0000

    Added Angular Material started a drag/drop tech item review component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QueryTableComponent } from './query-table/query-table.component';
@@ -18,9 +21,13 @@ import { TechExplorerComponent } from './tech-explorer/tech-explorer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+      AppComponent
+  ]
 })
 export class AppModule { }
--- src/app/tech-explorer/tech-explorer.component.ts
+++ src/app/tech-explorer/tech-explorer.component.ts
@@ -1,16 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 
+import {
+    CdkDragDrop, moveItemInArray, transferArrayItem
+} from '@angular/cdk/drag-drop';
+
 @Component({
-  selector: 'tech-explorer',
-  templateUrl: './tech-explorer.component.html',
-  styleUrls: ['./tech-explorer.component.scss']
+    selector: 'tech-explorer',
+    templateUrl: './tech-explorer.component.html',
+    styleUrls: ['./tech-explorer.component.scss']
 })
 export class TechExplorerComponent implements OnInit {
 
-  constructor() { }
+    states = ["hold", "assess", "trial", "adopt", "exhaust"];
 
-  ngOnInit(): void {
-  }
+    labels : { [key : string] : string } = {
+	hold: "Hold",
+	assess: "Assess",
+	trial: "Trial",
+	adopt: "Adopt",
+	exhaust: "Exhaust",
+    };
+
+    items : { [key : string] : string[] } = {
+	hold: ['Carrots', 'Tomatoes', 'Onions', 'Apples', 'Avocados'],
+	assess: ['Oranges', 'Bananas', 'Cucumbers'],
+	trial: [],
+	adopt: [],
+	exhaust: [],
+    };
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    drop(event: CdkDragDrop<string[]>) {
+	if (event.previousContainer === event.container) {
+	    moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+	} else {
+	    transferArrayItem(
+		event.previousContainer.data,
+		event.container.data,
+		event.previousIndex,
+		event.currentIndex,
+	    );
+	}
+    }
+
+    dump() {
+	console.log(this.items);
+    }
 
 }
+
+
 
