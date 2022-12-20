38e1682c173bc33439f594a2715b59013e31ba0f
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-12-20 21:08:17 +0000

    Property handling works

--- src/app/graph-viewer/graph-viewer/graph-viewer.component.ts
+++ src/app/graph-viewer/graph-viewer/graph-viewer.component.ts
@@ -37,99 +37,6 @@ export class GraphViewerComponent implements OnInit {
 
     fetchEdges = 25;
 
-    updateProperties() {
-
-	if (this.selected == undefined) return;
-
-	this.selectedLink = undefined;
-	this.selectedLabel = undefined
-	this.selectedThumbnail = undefined;
-
-	this.query.query(
-	    new Query(
-		"Fetch " + this.selected,
-		this.selected,
-		undefined,
-		undefined,
-		this.fetchEdges,
-	    )
-	).subscribe(
-	    res => {
-
-		let properties : { [key : string] : string } = {};
-		let title = "n/a";
-
-		try {
-
-		    for (let row of res) {
-
-			if (row.p == LABEL) {
-			    this.selectedLabel = row.o.value;
-			}
-
-			if (row.p == THUMBNAIL) {
-			    this.selectedThumbnail = row.o.value;
-			    continue;
-			}
-
-			if (row.p == IS_A) {
-			    this.query.query(
-				new Query(
-				    "Label " + row.o.value,
-				    row.o.value, LABEL, undefined,
-				    4 // FIXME: only need 1
-				)
-			    ).subscribe(
-				res => {
-				    try{
-					properties["class"] = res[0].o.value;
-				    } catch {
-				    }
-				}
-			    );
-			}
-
-			if (row.p == RELATION) {
-			    this.selectedLink = row.o.value;
-			}
-
-			if (row.o.uri) continue;
-
-			this.query.query(
-			    new Query(
-				"Label " + row.p,
-				row.p, LABEL, undefined,
-				4 // FIXME: only need 1
-			    )
-			).subscribe(
-			    res => {
-
-				let label;
-
-				try{
-				    label = res[0].o.value;
-				} catch {
-				    label = this.makeLabel(row.p);
-				}
-
-				properties[label] = row.o.value;
-			    }
-			);
-
-		    }
-
-		    this.properties = properties;
-
-		} catch (e) {
-		    console.log(e);
-		    this.selectedLabel = "n/a";
-		    this.properties = {};
-		}
-	    }
-	);
-
-    }
-
     ngOnInit(): void {
 
 	this.route.queryParams.subscribe(
@@ -161,9 +68,13 @@ export class GraphViewerComponent implements OnInit {
 
 	this.graph.nodeSelectEvents().subscribe(
 	    ev => {
-		if (ev.node.id == this.selected) return;
+
 		this.selected = ev.node.id;
-		this.updateProperties();
+
+		if (ev.node.label)
+		    this.selectedLabel = ev.node.label;
+		else
+		    this.selectedLabel = "-- undefined --";
 
 	    }
 	);
@@ -174,6 +85,24 @@ export class GraphViewerComponent implements OnInit {
 		this.selected = undefined;
 		this.selectedLabel = undefined;
 		this.selectedLink = undefined;
+	    }
+	);
+
+	this.graph.propertiesEvents().subscribe(
+	    ev => {
+
+		this.properties = {};
+		this.selectedThumbnail = undefined;
+		this.selectedLink = undefined;
+
+		for (let key in ev.properties) {
+		    if (key == "thumbnail")
+			this.selectedThumbnail = ev.properties[key];
+		    if (key == "link")
+			this.selectedLink = ev.properties[key];
+		    else
+			this.properties[key] = ev.properties[key];
+		}
 	    }
 	);
 
--- src/app/graph-viewer/graph.service.ts
+++ src/app/graph-viewer/graph.service.ts
@@ -48,184 +48,14 @@ export class RecentreEvent {
     expand : string = "false";
 };
 
+export class Properties {
+    properties : { [key : string] : string } = {};
+};
+
 @Injectable({
     providedIn: 'root'
 })
 export class GraphService {
-
-    fetchEdges = 25;
-
-    private addNodeSubject = new Subject<AddNodeEvent>;
-    private removeNodeSubject = new Subject<RemoveNodeEvent>;
-    private addEdgeSubject = new Subject<AddEdgeEvent>;
-    private removeEdgeSubject = new Subject<RemoveEdgeEvent>;
-    private nodeSelectSubject = new Subject<NodeSelectEvent>;
-    private nodeDeselectSubject = new Subject<null>;
-    private resetSubject = new Subject<null>;
-    private recentreSubject = new BehaviorSubject<RecentreEvent>(
-       new RecentreEvent()
-    );
-    private schemaSubject = new Subject<null>;
-
-    addNodeEvents() { return this.addNodeSubject; }
-    removeNodeEvents() { return this.removeNodeSubject; }
-    addEdgeEvents() { return this.addEdgeSubject; }
-    removeEdgeEvents() { return this.removeEdgeSubject; }
-    nodeSelectEvents() { return this.nodeSelectSubject; }
-    nodeDeselectEvents() { return this.nodeDeselectSubject; }
-    resetEvents() { return this.resetSubject; }
-    recentreEvents() { return this.recentreSubject; }
-    schemaEvents() { return this.schemaSubject; }
-
-    addNode(node : Node) {
-	let ev = new AddNodeEvent();
-	ev.node = node;
-	this.addNodeSubject.next(ev);
-    }
-
-    removeNode(id : string) {
-	let ev = new RemoveNodeEvent();
-	ev.id = id;
-	this.removeNodeSubject.next(ev);
-    }
-
-    addEdge(edge : Edge) {
-	let ev = new AddEdgeEvent();
-	ev.edge = edge;
-	this.addEdgeSubject.next(ev);
-    }
-
-    removeEdge(id : string) {
-	let ev = new RemoveEdgeEvent();
-	ev.id = id;
-	this.removeEdgeSubject.next(ev);
-    }
-
-    select(n : Node) {
-	let ev = new NodeSelectEvent();
-	ev.node = n;
-
-	console.log("SELECT FIXME");
-	this.getProperties(n);
-	
-	this.nodeSelectSubject.next(ev);
-    }
-
-    deselect() {
-	this.nodeDeselectSubject.next(null);
-    }
-
-    reset() {
-	this.resetSubject.next(null);
-    }
-
-    recentre(id : string, expand : string = "no") {
-	let ev = new RecentreEvent();
-	ev.id = id;
-	ev.expand = expand;
-	this.recentreSubject.next(ev);
-    }
-
-    schema() {
-	this.schemaSubject.next(null);
-    }
-
-    includeTriples(triples : Triple[]) {
-
-	for (let triple of triples) {
-
-	    if (triple.o.uri) {
-
-		// Edge points to object
-
-		// Ignore relation links, point to e.g. a web resource
-		if (triple.p == RELATION) continue;
-
-		// Ignore thumbnail links, point to a thumbnail image
-		if (triple.p == THUMBNAIL) continue;
-
-		this.includeNode(triple.s);
-		this.includeNode(triple.o.value);
-		this.includeEdge(triple.s, triple.p, triple.o.value);
-
-	    } else {
-
-		// Just a property, do nothing.
-
-	    }
-	    
-	}
-	
-    }
-    
-    includeNode(id : string) {
-
-	let n = new Node();
-	n.id = id;
-
-	this.query.query(
-	    new Query(
-		"Label " + id,
-		id,
-		LABEL,
-		undefined,
-		4 // FIXME: only need 1
-	    )
-	).subscribe(
-	    ev => {
-		try {
-		    n.label = ev[0].o.value;
-		} catch {
-		    n.label = this.makeLabel(id);
-		}
-		this.addNode(n);
-	    }
-	);
-	
-    }
-
-    includeEdge(from : string, rel : string, to : string) {
-	
-	let link = new Edge();
-	link.id = from + "//" + rel + "//" + to;
-	link.from = from;
-	link.to = to;
-
-	this.query.query(
-	    new Query(
-		"Label " + rel,
-		rel,
-		LABEL,
-		undefined,
-		4 // FIXME: Only need 1
-	    )
-	).subscribe(
-	    ev => {
-		try {
-		    link.label = ev[0].o.value;
-		} catch {
-		    link.label = this.makeLabel(rel);
-		}
-		this.addEdge(link);
-	    }
-	);
-
-    }
-
-    makeLabel(label : string) {
-
-	if (label.startsWith("http://"))
-            label = label.substr(label.lastIndexOf("/") + 1);
-
-	if (label.lastIndexOf("#") >= 0)
-            label = label.substr(label.lastIndexOf("#") + 1);
-
-	if (label.length > 20)
-	    label = label.substring(0, 30);
-
-	return label;
-
-    }
 
     constructor(
 	private command : CommandService,
@@ -346,6 +176,184 @@ export class GraphService {
 
 	    }
 	);
+
+	this.nodeSelectEvents().subscribe(
+	    (ev : any) => {
+		this.getProperties(ev.node);
+	    }
+	);
+
+    }
+
+    fetchEdges = 25;
+
+    private addNodeSubject = new Subject<AddNodeEvent>;
+    private removeNodeSubject = new Subject<RemoveNodeEvent>;
+    private addEdgeSubject = new Subject<AddEdgeEvent>;
+    private removeEdgeSubject = new Subject<RemoveEdgeEvent>;
+    private nodeSelectSubject = new Subject<NodeSelectEvent>;
+    private nodeDeselectSubject = new Subject<null>;
+    private resetSubject = new Subject<null>;
+    private recentreSubject = new BehaviorSubject<RecentreEvent>(
+       new RecentreEvent()
+    );
+    private schemaSubject = new Subject<null>;
+    private propertiesSubject = new Subject<Properties>;
+
+    addNodeEvents() { return this.addNodeSubject; }
+    removeNodeEvents() { return this.removeNodeSubject; }
+    addEdgeEvents() { return this.addEdgeSubject; }
+    removeEdgeEvents() { return this.removeEdgeSubject; }
+    nodeSelectEvents() { return this.nodeSelectSubject; }
+    nodeDeselectEvents() { return this.nodeDeselectSubject; }
+    resetEvents() { return this.resetSubject; }
+    recentreEvents() { return this.recentreSubject; }
+    schemaEvents() { return this.schemaSubject; }
+    propertiesEvents() { return this.propertiesSubject; }
+
+    addNode(node : Node) {
+	let ev = new AddNodeEvent();
+	ev.node = node;
+	this.addNodeSubject.next(ev);
+    }
+
+    removeNode(id : string) {
+	let ev = new RemoveNodeEvent();
+	ev.id = id;
+	this.removeNodeSubject.next(ev);
+    }
+
+    addEdge(edge : Edge) {
+	let ev = new AddEdgeEvent();
+	ev.edge = edge;
+	this.addEdgeSubject.next(ev);
+    }
+
+    removeEdge(id : string) {
+	let ev = new RemoveEdgeEvent();
+	ev.id = id;
+	this.removeEdgeSubject.next(ev);
+    }
+
+    select(n : Node) {
+	let ev = new NodeSelectEvent();
+	ev.node = n;
+	this.nodeSelectSubject.next(ev);
+    }
+
+    deselect() {
+	this.nodeDeselectSubject.next(null);
+    }
+
+    reset() {
+	this.resetSubject.next(null);
+    }
+
+    recentre(id : string, expand : string = "no") {
+	let ev = new RecentreEvent();
+	ev.id = id;
+	ev.expand = expand;
+	this.recentreSubject.next(ev);
+    }
+
+    schema() {
+	this.schemaSubject.next(null);
+    }
+
+    includeTriples(triples : Triple[]) {
+
+	for (let triple of triples) {
+
+	    if (triple.o.uri) {
+
+		// Edge points to object
+
+		// Ignore relation links, point to e.g. a web resource
+		if (triple.p == RELATION) continue;
+
+		// Ignore thumbnail links, point to a thumbnail image
+		if (triple.p == THUMBNAIL) continue;
+
+		this.includeNode(triple.s);
+		this.includeNode(triple.o.value);
+		this.includeEdge(triple.s, triple.p, triple.o.value);
+
+	    } else {
+
+		// Just a property, do nothing.
+
+	    }
+	    
+	}
+	
+    }
+    
+    includeNode(id : string) {
+
+	let n = new Node();
+	n.id = id;
+
+	this.query.query(
+	    new Query(
+		"Label " + id,
+		id,
+		LABEL,
+		undefined,
+		4 // FIXME: only need 1
+	    )
+	).subscribe(
+	    ev => {
+		try {
+		    n.label = ev[0].o.value;
+		} catch {
+		    n.label = this.makeLabel(id);
+		}
+		this.addNode(n);
+	    }
+	);
+	
+    }
+
+    includeEdge(from : string, rel : string, to : string) {
+	
+	let link = new Edge();
+	link.id = from + "//" + rel + "//" + to;
+	link.from = from;
+	link.to = to;
+
+	this.query.query(
+	    new Query(
+		"Label " + rel,
+		rel,
+		LABEL,
+		undefined,
+		4 // FIXME: Only need 1
+	    )
+	).subscribe(
+	    ev => {
+		try {
+		    link.label = ev[0].o.value;
+		} catch {
+		    link.label = this.makeLabel(rel);
+		}
+		this.addEdge(link);
+	    }
+	);
+
+    }
+
+    makeLabel(label : string) {
+
+	if (label.startsWith("http://"))
+            label = label.substr(label.lastIndexOf("/") + 1);
+
+	if (label.lastIndexOf("#") >= 0)
+            label = label.substr(label.lastIndexOf("#") + 1);
+
+	if (label.length > 20)
+	    label = label.substring(0, 30);
+
+	return label;
 
     }
 
@@ -485,16 +493,33 @@ export class GraphService {
 
 		let todo = this.mapToProperties(res);
 
-		// forkJoin on empty set is bad
-		if (todo === {})
+		// Empty set, return empty properties.
+		// Also, forkJoin on empty set is bad.
+		if (todo === {}) {
+		    let ev = new Properties();
+		    ev.properties = {};
+		    this.propertiesSubject.next(ev);
 		    return;
+		}
 
 		forkJoin(todo).subscribe(
 		    (res : { [key : string] : any }) => {
-			console.log("FORKJOIN DONE");
+
+			let props : { [key : string] : string } = {};
+
 			for (let i in res) {
-			    console.log(res[i]);
+
+			    if (res[i].length == 0)
+				continue;
+
+			    props[res[i][0]] = res[i][1];
+
 			}
+
+			let ev = new Properties();
+			ev.properties = props;
+			this.propertiesSubject.next(ev);
+			
 		    }
 		);
 
