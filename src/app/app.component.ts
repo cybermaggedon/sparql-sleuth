447b1b62b21dd2c8419b2c18ba5c0fd27fe01b20
Cyber MacGeddon <cybermaggedon@gmail.com>
2023-01-01 13:01:43 +0000

    Small info box shows activity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,14 +13,25 @@ export class AppComponent implements OnInit {
 
     working = false;
 
+    info : string[] = [];
+
     constructor(
         private progress : ProgressService,
     ) {
 	this.progress.progressEvents().pipe(
+	    // This skips a whole heap of events means some stuff won't be
+	    // shown on the screen, but accuracy isn't important here, more
+	    // the indication of activity.
 	    debounceTime(10),
 	).subscribe(
 	    p => {
+
 		this.working = p.working();
+
+		this.info = Array.from(p.progress.values()).slice(0, 4).map(
+		    x => x + "..."
+		);
+
 	    }
 	);
     }
