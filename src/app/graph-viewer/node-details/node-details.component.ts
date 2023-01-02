6c8fcedeff99753ea593dc039d4311e8ccfa7856
Cyber MacGeddon <cybermaggedon@gmail.com>
2023-01-02 11:04:50 +0000

    Remove need for detail and node-details

--- src/app/graph-viewer/node-details/node-details.component.ts
+++ src/app/graph-viewer/node-details/node-details.component.ts
@@ -1,8 +1,8 @@
 
 import { Component, OnInit, Input } from '@angular/core';
 
-import { RelationshipService } from '../../graph/relationship.service';
-import { Properties } from '../../graph/properties.service';
+import { GraphService } from '../../graph/graph.service';
+import { Properties, PropertyMap } from '../../graph/properties.service';
 import { Node, Relationship } from '../../graph/graph';
 import { CommandService, Direction } from '../../command.service';
 
@@ -13,22 +13,88 @@ import { CommandService, Direction } from '../../command.service';
 })
 export class NodeDetailsComponent implements OnInit {
 
-    constructor(
-	private command : CommandService
-    ) {
-    }
+    selected : string | undefined;
+    selectedLabel : string | undefined;
+    selectedThumbnail : string | undefined;
+    selectedLink : string | undefined;
 
     @Input()
     relationships : Relationship[] = [];
 
     @Input()
-    properties : Properties = new Properties();
-
-    @Input()
     selection? : Node;
+
+    @Input("properties") allProperties : Properties = new Properties();
+
+    get inward() {
+	return this.relationships.filter( x => x.inward );
+    }
+
+    get outward() {
+	return this.relationships.filter( x => !x.inward );
+    }
+
+    get properties() {
+	return this.allProperties.properties;
+    }
+
+    noProperties() : boolean {
+	return Object.keys(this.properties).length == 0;
+    }
+
+    get table() {
+
+	let rows : {key: string, value: string}[] = [];
+
+	for(let key in this.properties) {
+	    rows.push({
+		key: key, value: this.properties[key].value()
+	    });
+	}
+	
+	return rows;
+
+    }
+
+    constructor(
+	private graph : GraphService,
+	private command : CommandService,
+    ) { }
 
     ngOnInit(): void {
     }
 
-}
+    get label() : string {
+	if ("title" in this.properties) {
+	    return this.properties["title"].value();
+	} else if ("label" in this.properties) {
+	    return this.properties["label"].value();
+	} else
+	    return "";
+    }
+    
+    get link() : string {
+	if ("link" in this.properties)
+	    return this.properties["link"].value();
+	else
+	    return "";
+    }
 
+    get thumbnail() : string {
+	if ("thumbnail" in this.properties)
+	    return this.properties["thumbnail"].value();
+	else
+	    return "";
+    }
+
+    recentre() {
+	if (this.selection)
+	    this.command.recentre(this.selection.id);
+    }
+
+    reln(rel : Relationship) {
+	if (!this.selection) return;
+	this.command.relationship(this.selection, rel);
+    }
+
+}
